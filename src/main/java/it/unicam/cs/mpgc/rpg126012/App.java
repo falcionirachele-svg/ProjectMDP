f6211099cbd0e164a7bc48429ea2f1424ec7068b
falcionirachele-svg <falcionirachele@gmail.com>
2026-06-13 12:47:47 +0200

    implementata la main class app per avviare il gioco

--- src/main/java/it/unicam/cs/mpgc/rpg126012/App.java
+++ src/main/java/it/unicam/cs/mpgc/rpg126012/App.java
@@ -1,5 +1,12 @@
 package it.unicam.cs.mpgc.rpg126012;
 
+import it.unicam.cs.mpgc.rpg126012.Controller.ControllerSceneBuilder.GameControllerFX;
+import it.unicam.cs.mpgc.rpg126012.Controller.ControllerTerminale.GameController;
+import it.unicam.cs.mpgc.rpg126012.Model.Player;
+import it.unicam.cs.mpgc.rpg126012.Model.StoryData;
+import it.unicam.cs.mpgc.rpg126012.Model.StoryLoader;
+import it.unicam.cs.mpgc.rpg126012.Model.StoryNode;
+import it.unicam.cs.mpgc.rpg126012.View.ViewTerminale.GameView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,7 +14,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception {}
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            //carcio la storia del json
+            StoryLoader storyLoader = new StoryLoader();
+            //gli passo il file json e carico la storia
+            StoryData storia = storyLoader.load("src/main/resources/NodeStory.json");
+            //mi salvo il nodo di partenza per usarlo nel controller
+            StoryNode nodoPartenza = storia.getNodoIniziale();
+            //carico il file fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/SchermataGioco.fxml"));
+            Parent root = loader.load();
+            //prendo il controller dal file fxml
+            GameControllerFX controller = loader.getController();
+            //creo il giocatore
+            Player player = new Player("Giocatore",180,20);
+            //inizio il gioco
+            controller.startGame(nodoPartenza, player);
+            //creo la scena e la setto
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            System.err.println("Errore durante il caricamento della storia");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         // Lancia l'applicazione JavaFX
         launch(args);
